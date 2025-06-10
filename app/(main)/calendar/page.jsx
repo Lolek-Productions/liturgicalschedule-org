7b3899b921dffcbf8609b1f0ffa94eadd591415c
Josh McCarty <fr.mccarty@gmail.com>
2025-06-10 16:38:58 -0500

    feat: add calendar page with react-big-calendar integration

---
+++ app/(main)/calendar/page.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+const myEventsList = [
+  {
+    title: "Sample Event",
+    start: new Date(),
+    end: new Date(new Date().getTime() + 60 * 60 * 1000),
+  },
+];
+
+export default function CalendarPage() {
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+}
